d2cad446f5c62ca7b1b6470b11117c37327e98e9
ra <camerondiaz1215@gmail.com>
2023-01-26 23:06:06 +0900

    環境構築

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+    return (
+        <main>
+            <div className="m-10 text-center">
+                <p>Hello World🚀</p>
+            </div>
+        </main>
+    )
+}
