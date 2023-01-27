25efb9360019fc1783854e0a30a587bb63a8b2be
ra <camerondiaz1215@gmail.com>
2023-01-27 19:29:31 +0900

    Navbarデザイン修正

--- app/components/nav-bar.tsx
+++ app/components/nav-bar.tsx
@@ -11,6 +11,7 @@ export default function NavBar() {
         supabase.auth.signOut()
         resetLoginUser()
     }
+
     const LoggedIn = ({ loginUser }: any) => {
         if (
             loginUser &&
@@ -20,41 +21,46 @@ export default function NavBar() {
         ) {
             return (
                 <>
-                    {' '}
-                    <Link
-                        href={`/auth/my-page/${loginUser.id}`}
-                        className="rounded bg-gray-700 px-3 py-2 text-white hover:bg-gray-500"
-                    >
-                        MyPage
-                    </Link>
-                    <Link
-                        href="/auth"
-                        className="rounded bg-gray-700 px-3 py-2 text-white hover:bg-gray-500"
-                    >
-                        Auth with CRUD
-                    </Link>
-                    <ArrowRightOnRectangleIcon
-                        className="my-6 h-6 w-6 cursor-pointer text-blue-500"
-                        onClick={signOut}
-                    />
+                    <div className="text-sm lg:flex-grow">
+                        <Link
+                            href={`/auth/my-page/${loginUser.id}`}
+                            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-teal-200 mr-4"
+                        >
+                            MyPage
+                        </Link>
+                        <Link
+                            href="/auth"
+                            className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-teal-200"
+                        >
+                            TODO
+                        </Link>
+                    </div>
+                    <div>
+                        <ArrowRightOnRectangleIcon
+                            className="inline-block my-1 h-6 w-6 cursor-pointer lg:mt-0 text-white hover:bg-teal-500"
+                            onClick={signOut}
+                        />
+                    </div>
                 </>
             )
         }
         return null
     }
-    useEffect(() => {
-        LoggedIn({ loginUser })
-    }, [loginUser])
+
     return (
-        <header className="bg-gray-800 p-4">
-            <nav className="space-x-4">
+        <header className="bg-neutral-800 p-2">
+            <nav className="flex items-center justify-between flex-wrap p-6">
                 <Link
                     href="/"
-                    className="rounded bg-gray-700 px-3 py-2 text-white hover:bg-gray-500"
+                    className="flex items-center flex-shrink-0 text-white mr-6 hover:text-teal-200"
                 >
-                    Home
+                    <span className="font-semibold text-xl tracking-tight">
+                        Phone Manegement
+                    </span>
                 </Link>
-                <LoggedIn loginUser={loginUser} />
+                <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+                    <LoggedIn loginUser={loginUser} />
+                </div>
             </nav>
         </header>
     )
