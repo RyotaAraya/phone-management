329c011207d8a19104bbf089c0d06888febf1639
ra <camerondiaz1215@gmail.com>
2023-01-27 16:06:13 +0900

    nav 未ログインだと少なめ表示

--- app/components/nav-bar.tsx
+++ app/components/nav-bar.tsx
@@ -1,18 +1,20 @@
 'use client'
 import Link from 'next/link'
 import useStore from '../../store'
+import { useEffect } from 'react'
 
 export default function NavBar() {
     const { loginUser } = useStore()
-    return (
-        <header className="bg-gray-800 p-4">
-            <nav className="space-x-4">
-                <Link
-                    href="/"
-                    className="rounded bg-gray-700 px-3 py-2 text-white hover:bg-gray-500"
-                >
-                    Home
-                </Link>
+    const LoggedIn = ({ loginUser }: any) => {
+        if (
+            loginUser.id === '' ||
+            loginUser.id === undefined ||
+            loginUser.id === null
+        )
+            return null
+        return (
+            <>
+                {' '}
                 <Link
                     href={`/auth/my-page/${loginUser.id}`}
                     className="rounded bg-gray-700 px-3 py-2 text-white hover:bg-gray-500"
@@ -25,6 +27,22 @@ export default function NavBar() {
                 >
                     Auth with CRUD
                 </Link>
+            </>
+        )
+    }
+    useEffect(() => {
+        LoggedIn({ loginUser })
+    }, [loginUser])
+    return (
+        <header className="bg-gray-800 p-4">
+            <nav className="space-x-4">
+                <Link
+                    href="/"
+                    className="rounded bg-gray-700 px-3 py-2 text-white hover:bg-gray-500"
+                >
+                    Home
+                </Link>
+                <LoggedIn loginUser={loginUser} />
             </nav>
         </header>
     )
