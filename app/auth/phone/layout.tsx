7d2296cbe21bb025df81d5ce462b134071cf05ce
ra <camerondiaz1215@gmail.com>
2023-01-28 22:04:58 +0900

    phone修正

--- app/auth/phone/layout.tsx
+++ app/auth/phone/layout.tsx
@@ -11,6 +11,11 @@ export default async function PhoneLayout({
   return (
     <section className="flex">
       <aside className={`h-[calc(100vh-56px)] w-1/4 bg-gray-200`}>
+        <EditTask />
+        <Suspense fallback={<Spinner />}>
+          {/* @ts-ignore*/}
+          <TodoList />
+        </Suspense>
       </aside>
       <main className="flex flex-1 justify-center">{children}</main>
     </section>
--- app/auth/phone/page.tsx
+++ app/auth/phone/page.tsx
@@ -1,9 +1,9 @@
 export default function PhonePage() {
-    return (
-        <div className="m-10">
-            <span className="text-lg">
-                Phone Page 🚀
-            </span>
-        </div>
-    )
+  return (
+    <div className="m-10">
+      <span className="text-lg">
+        Phone Page
+      </span>
+    </div>
+  )
 }
