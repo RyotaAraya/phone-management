2338362c35e60d9cbfe3875af25d73f60b662b60
ra <camerondiaz1215@gmail.com>
2023-01-30 15:16:11 +0900

    auth見た目修正

--- app/components/auth-form.tsx
+++ app/components/auth-form.tsx
@@ -43,10 +43,6 @@ export default function AuthForm() {
     return (
         <div className="flex flex-col items-center justify-center">
             <p>{loginUser.email}</p>
-            <ArrowRightOnRectangleIcon
-                className="my-6 h-6 w-6 cursor-pointer text-blue-500"
-                onClick={signOut}
-            />
             <form onSubmit={handleSubmit}>
                 <div>
                     <input
@@ -75,7 +71,7 @@ export default function AuthForm() {
                 <div className="my-6 flex justify-center text-sm">
                     <button
                         type="submit"
-                        className=" rounded-md bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700"
+                        className="rounded-md bg-indigo-800 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700"
                     >
                         {isLogin ? 'Login' : 'Register'}
                     </button>
@@ -83,9 +79,9 @@ export default function AuthForm() {
             </form>
             <p
                 onClick={() => setIsLogin(!isLogin)}
-                className="cursor-pointer font-medium hover:text-indigo-500"
+                className="my-10 cursor-pointer font-medium hover:text-indigo-800"
             >
-                change mode ?
+                {isLogin ? 'Register?' : 'Login?'}
             </p>
         </div>
     )
